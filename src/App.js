81dc96344f007de73b15bf3a63da2189bc9fb4d3
rasaighul <afc5865@psu.edu>
2023-02-07 18:00:29 -0500

    initials

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 
 function App() {
   return (
-
-<>
-    <div class="outline">
-      <div class="lecard">
-        <h2>Some Nerd</h2>
-        <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
-        <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
-      </div>
-    </div>
-  <div class="buttons">
-  
-  <button class="copyCat">Copy</button>
-  <button id="del">Delete</button>
-  <button class="btnDet">Toggle Details</button> 
-  <button class="btnCT">Change Title</button>
-  <button  class="btnBG">Change Background Color</button>
-  
+<div>
+  <div class='buttons'>
+    <button id="addToButton" class="buttons"> Add Onto </button>
+    <button id="deleteButton" class='buttons'> Delete Last</button>
+    <button id="changeBackgroundButton" class='buttons'> Change Background</button>
+    <button id="changeHeadingButton" class='buttons'> Change Heading</button>
+    <button data-toggle-button class = 'buttons'> Toggle Details </button>
   </div>
   
-</>
+  <div class="outsidediv">
+      <img class="hingle" src="https://pbs.twimg.com/profile_images/1288205058585300992/eH8L4MYG_400x400.jpg" />
+      <div id='toChange'>
+        <h1 class="titleCardHingle" id='title'> Hingle McCringleberry </h1>
+        <div class="outside">
+          <details class ='details'>
+            <summary>Details</summary>
+            <ul class="information">
+              <p>Hingle McCringleberry is the 2012 Heisman-winning tight end out of Penn State University, selected No. 1 overall by the Rhinos in the 2016 NFL Draft.
+              </p>
+            </ul>
+            </details>
+        </div>
+      </div>
+  </div>
+</div>
   );
 }
 
